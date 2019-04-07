469816a99200bac1fe184a6cf28d89a067208144
Idris Adeniji <idrisadeniji01@gmail.com>
2019-04-07 17:45:57 -0400

    Create dashboard page.

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
-require_once 'includes/Database.php';
+require_once 'includes/UserClass.php';
 
-$connect = new Database();
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TaskManager :: Dashboard</title>
+</head>
+<body>
+    
+</body>
+</html>

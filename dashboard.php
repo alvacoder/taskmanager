4bb540456f205411ed3ddc032d9ed70d0c91c616
Idris Adeniji <idrisadeniji01@gmail.com>
2019-04-19 04:51:21 -0400

    user dashboard page

---
+++ dashboard.php
@@ -0,0 +1,11 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
+<script type ="text/javascript">
+   $(function() {
+      Swal.fire({
+         'title':'Welcome Back',
+         'text': 'Ensure to keep your task status updated regularly.',
+         'type': 'success'
+      });
+   });
+</script>
